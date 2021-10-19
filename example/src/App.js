8ef7d208bae92ae33f669238ef5854366a6a1c87
sergioedo <sergio.edo@gmail.com>
2021-10-19 19:34:38 +0200

    add poi sample with EPSG:4326

--- example/src/App.js
+++ example/src/App.js
@@ -1,15 +1,16 @@
 import * as React from "react"
 import { Admin, Resource } from "react-admin"
 import geoServerProvider from "ra-data-geoserver"
-import { ArchSiteList, ArchSiteEdit, ArchSiteShow } from "./archsites"
+// import { ArchSiteList, ArchSiteEdit, ArchSiteShow } from "./archsites"
+import { PoiList, PoiEdit, PoiShow } from "./poi"
 import RoomIcon from "@material-ui/icons/Room"
 
 const dataProvider = geoServerProvider({
     geoserverBaseURL: "http://localhost:3000/geoserver",
-    geoserverWorkspace: "sf",
-    extraQueryParams: {
-        cql_filter: "cat<=15",
-    },
+    geoserverWorkspace: "tiger",
+    // extraQueryParams: {
+    //     cql_filter: "cat<=15",
+    // },
     // flattenProperties: false,
     geoserverUser: process.env.REACT_APP_GEOSERVER_USER,
     geoserverPassword: process.env.REACT_APP_GEOSERVER_PASSWORD,
@@ -17,12 +18,12 @@ const dataProvider = geoServerProvider({
 const App = () => (
     <Admin title="GeoServer Admin" dataProvider={dataProvider}>
         <Resource
-            name="archsites"
+            name="poi"
             icon={RoomIcon}
-            options={{ label: "Archeological Sites" }}
-            list={ArchSiteList}
-            show={ArchSiteShow}
-            edit={ArchSiteEdit}
+            options={{ label: "Manhattan POIs" }}
+            list={PoiList}
+            show={PoiShow}
+            edit={PoiEdit}
         />
     </Admin>
 )
---
+++ example/src/poi.js
@@ -0,0 +1,99 @@
+import * as React from "react"
+import {
+    List,
+    Edit,
+    Show,
+    Datagrid,
+    SimpleForm,
+    TextField,
+    NumberField,
+    TextInput,
+    SimpleShowLayout,
+    useShowController,
+} from "react-admin"
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet"
+
+export const PoiList = (props) => (
+    <List {...props}>
+        <Datagrid rowClick="show">
+            <TextField source="id" label={"ID"} />
+            <TextField source="properties.NAME" label={"Name"} />
+            <TextField source="properties.THUMBNAIL" label={"Thumbnail"} />
+            <TextField source="properties.MAINPAGE" label={"Main Page"} />
+        </Datagrid>
+    </List>
+)
+
+export const PoiEdit = (props) => (
+    <Edit {...props}>
+        <SimpleForm>
+            {/* <TextInput source="id" label={'ID'} /> */}
+            <TextInput source="properties.NAME" label={"Name"} />
+            <TextInput source="properties.THUMBNAIL" label={"Thumbnail"} />
+            <TextInput source="properties.MAINPAGE" label={"Main Page"} />
+            <TextInput source="geometry.coordinates[1]" label={"Latitude"} />
+            <TextInput source="geometry.coordinates[0]" label={"Longitude"} />
+        </SimpleForm>
+    </Edit>
+)
+
+export const PoiShow = (props) => {
+    console.log(props)
+    const {
+        // basePath, // deduced from the location, useful for action buttons
+        // defaultTitle, // the translated title based on the resource, e.g. 'Post #123'
+        // loaded, // boolean that is false until the record is available
+        // loading, // boolean that is true on mount, and false once the record was fetched
+        record, // record fetched via dataProvider.getOne() based on the id from the location
+        // resource, // the resource name, deduced from the location. e.g. 'posts'
+        // version, // integer used by the refresh feature
+    } = useShowController(props)
+    console.log(record)
+    const lat = record && record.geometry.coordinates[1]
+    const lon = record && record.geometry.coordinates[0]
+    return (
+        <Show {...props}>
+            <SimpleShowLayout>
+                {/* <TextField source="id" /> */}
+                <TextField source="properties.NAME" label={"Name"} />
+                <TextField source="properties.THUMBNAIL" label={"Thumbnail"} />
+                <TextField source="properties.MAINPAGE" label={"Main Page"} />
+                <TextField source="geometry.type" label={"Geometry"} />
+                <NumberField
+                    source="geometry.coordinates[1]"
+                    label={"Latitude"}
+                    options={{
+                        style: "decimal",
+                        maximumFractionDigits: 6,
+                    }}
+                />
+                <NumberField
+                    source="geometry.coordinates[0]"
+                    label={"Longitude"}
+                    options={{
+                        style: "decimal",
+                        maximumFractionDigits: 6,
+                    }}
+                />
+                {record && (
+                    <MapContainer
+                        style={{ height: "350px" }}
+                        center={[lat, lon]}
+                        zoom={16}
+                        scrollWheelZoom={true}
+                    >
+                        <TileLayer
+                            attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+                            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                        />
+                        <Marker position={[lat, lon]}>
+                            <Popup>
+                                A pretty CSS3 popup. <br /> Easily customizable.
+                            </Popup>
+                        </Marker>
+                    </MapContainer>
+                )}
+            </SimpleShowLayout>
+        </Show>
+    )
+}
