6af696763fae951c94939fdc7671bf3d4ca3473a
jjsanchezfernandez <sanferju@gmail.com>
2025-11-18 14:16:09 +0100

    added landmark and roads features

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -3,11 +3,20 @@ import { Admin, Resource, ShowGuesser } from "react-admin";
 import geoServerProvider from "ra-data-geoserver";
 // import { PostList, PostEdit, PostCreate } from "./posts";
 import { PoiList/*, PoiCreate*/, PoiEdit, PoiShow } from "./resources/poi"
+import { RoadList/*, RoadCreate, RoadEdit*/, RoadShow } from "./resources/roads"
+import {
+    LandMarkList,
+    // LandMarkCreate,
+    // LandMarkEdit,
+    LandMarkShow
+} from "./resources/landmarks"
 // import { UserList } from "./users";
 import { Dashboard } from './Dashboard';
 import { authProvider } from './authProvider';
 
-
+import POIIcon from "@material-ui/icons/Room"
+import LineIcon from "@material-ui/icons/Timeline"
+import PolygonIcon from "@material-ui/icons/FormatShapes"
 import PostIcon from "@mui/icons-material/Book";
 import UserIcon from "@mui/icons-material/Group";
 
@@ -25,12 +34,30 @@ export const App = () => (
     <Admin authProvider={authProvider} dataProvider={dataProvider} dashboard={Dashboard} >
         <Resource
             name="poi"
-            // icon={POIIcon}
+            icon={POIIcon}
             options={{ label: "Manhattan POIs" }}
             list={PoiList}
             show={PoiShow}
             // create={PoiCreate}
             edit={PoiEdit}
+        />
+        <Resource
+            name="tiger_roads"
+            icon={LineIcon}
+            options={{ label: "Manhattan Roads" }}
+            list={RoadList}
+            show={RoadShow}
+        // create={RoadCreate}
+        // edit={RoadEdit}
+        />
+        <Resource
+            name="poly_landmarks"
+            icon={PolygonIcon}
+            options={{ label: "Manhattan LandMarks" }}
+            list={LandMarkList}
+            show={LandMarkShow}
+        // create={LandMarkCreate}
+        // edit={LandMarkEdit}
         />
     </Admin>
 );
